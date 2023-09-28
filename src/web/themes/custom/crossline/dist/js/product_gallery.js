aaa276924ed15e6101618341651419e89ac675d4
sharif <hs.sharif819@gmail.com>
2023-09-28 19:09:43 +0600

    configuration and design for collections page

---
+++ src/web/themes/custom/crossline/dist/js/product_gallery.js
@@ -0,0 +1,23 @@
+!((document, $) => {
+  'use strict';
+
+  $(document).ready(function () {
+    if ($('.menu-filtr-container').length) {
+      const simpleFilters = document.querySelectorAll('.simplefilter .fltr-controls');
+      Array.from(simpleFilters).forEach(node => node.addEventListener('click', function () {
+        simpleFilters.forEach(filter => filter.classList.remove('active'));
+        node.classList.add('active');
+      }));
+
+      // Expose this filterizr as a global for debugging
+      window.filterizr = new window.Filterizr('.menu-filtr-container', {
+        controlsSelector: '.fltr-controls',
+        gutterPixels: 30,
+        spinner: {
+          enabled: true
+        }
+      });
+    }
+  });
+})(document, jQuery);
+//# sourceMappingURL=product_gallery.js.map
