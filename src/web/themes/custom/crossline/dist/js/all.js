101e4651d773ed232a8478bcef6cbcff64f78a47
sharif <hs.sharif819@gmail.com>
2023-09-29 10:46:12 +0600

    header design

--- src/web/themes/custom/crossline/dist/js/all.js
+++ src/web/themes/custom/crossline/dist/js/all.js
@@ -40,30 +40,6 @@
 !((document, $) => {
   'use strict';
 
-  $(document).ready(function () {
-    if ($('.menu-filtr-container').length) {
-      const simpleFilters = document.querySelectorAll('.simplefilter .fltr-controls');
-      Array.from(simpleFilters).forEach(node => node.addEventListener('click', function () {
-        simpleFilters.forEach(filter => filter.classList.remove('active'));
-        node.classList.add('active');
-      }));
-
-      // Expose this filterizr as a global for debugging
-      window.filterizr = new window.Filterizr('.menu-filtr-container', {
-        controlsSelector: '.fltr-controls',
-        gutterPixels: 30,
-        spinner: {
-          enabled: true
-        }
-      });
-    }
-  });
-})(document, jQuery);
-//# sourceMappingURL=menus.js.map
-
-!((document, $) => {
-  'use strict';
-
   //Webform submission on vendor page
   $(document).on('click', '.path-product .training-details-container .training-btn', function (evt) {
     evt.preventDefault();
